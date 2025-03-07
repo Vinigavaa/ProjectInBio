2d537e31f62b24c0fd2744187d679d015497c805
Vinigavaa <vinigava14@gmail.com>
2025-03-06 23:28:28 -0300

    Create Hero Component to Landing Page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,13 @@
 import "./globals.css";
 
+import {Red_Hat_Display} from "next/font/google"
+
+//apenas instancio a fonte
+const redHatDisplay = Red_Hat_Display({
+  subsets: ["latin"],
+  weight: ["300", "400", "700"],
+})
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -8,8 +16,7 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <body
-        className={`antialiased`}
-      >
+        className={`${redHatDisplay.className} bg-background-primary text-content-body antialiased`}>
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
+import Hero from "./components/landing-page/hero";
+
 export default function Home() {
   return (
-    <div>Hello World</div>
+    <div className="max-w-7xl mx-auto border">
+      <Hero />
+    </div>
   );
 }
