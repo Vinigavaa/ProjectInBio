7544fe9ee4bca3c65e92768d8c69e37b08b81fd4
Vinigavaa <vinigava14@gmail.com>
2025-03-08 17:19:51 -0300

    add: component creation total-visits

--- app/components/commons/user-card.tsx
+++ app/components/commons/user-card.tsx
@@ -1,11 +1,11 @@
-import {Github, Instagram, Linkedin, Twitter, Plus } from "lucide-react"
+import { Github, Instagram, Linkedin, Twitter, Plus } from "lucide-react"
 import Button from "../ui/button"
-export default function UserCard(){
-    return(
+export default function UserCard() {
+    return (
         //card-geral
         <div className="w-[348px] flex flex-col gap-5 items-center p-5 border-white border-opacity-10 bg-[#121212] rounded-3xl text-white">
             {/*card-foto*/}
-            <div className="size-48"> 
+            <div className="size-48">
                 <img src="/me.jpg" className="rounded-full object-cover w-full h-full"></img>
             </div>
             {/*card-nome*/}
@@ -19,18 +19,17 @@ export default function UserCard(){
             <div className="flex flex-col gap-2 w-full">
                 <span className="uppercase text-xs font-medium">Links</span>
                 <div className="flex gap-3">
-                    <button className="p-3 rounded-xl bg-[#1E1E1E] hover:bg-[#2E2E2E]"><Github/></button>
-                    <button className="p-3 rounded-xl bg-[#1E1E1E] hover:bg-[#2E2E2E]"><Instagram/></button>
-                    <button className="p-3 rounded-xl bg-[#1E1E1E] hover:bg-[#2E2E2E]"><Linkedin/></button>
-                    <button className="p-3 rounded-xl bg-[#1E1E1E] hover:bg-[#2E2E2E]"><Twitter/></button>
-                    <button className="p-3 rounded-xl bg-[#1E1E1E] hover:bg-[#2E2E2E]"><Plus/></button>
+                    <button className="p-3 rounded-xl bg-[#1E1E1E] hover:bg-[#2E2E2E]"><Github /></button>
+                    <button className="p-3 rounded-xl bg-[#1E1E1E] hover:bg-[#2E2E2E]"><Instagram /></button>
+                    <button className="p-3 rounded-xl bg-[#1E1E1E] hover:bg-[#2E2E2E]"><Linkedin /></button>
+                    <button className="p-3 rounded-xl bg-[#1E1E1E] hover:bg-[#2E2E2E]"><Twitter /></button>
+                    <button className="p-3 rounded-xl bg-[#1E1E1E] hover:bg-[#2E2E2E]"><Plus /></button>
                 </div>
-                {/*card-botao*/}
-                <div className="flex flex-col gap-3 w-full h-[172px]">
-                    <div className="w-full flex flex-col items-center gap-3">
-                        <Button className="w-full">Template SaaS - Compre Agora</Button>
-                        <button className="p-3 rounded-xl bg-[#1E1E1E] hover:bg-[#2E2E2E]"><Plus/></button>
-                    </div>
+            </div>
+            <div className="flex flex-col gap-3 w-full h-[172px]">
+                <div className="w-full flex flex-col items-center gap-3">
+                    <Button className="w-full">Template SaaS - Compre Agora</Button>
+                    <button className="p-3 rounded-xl bg-[#1E1E1E] hover:bg-[#2E2E2E]"><Plus /></button>
                 </div>
             </div>
         </div>
--- app/components/landing-page/hero.tsx
+++ app/components/landing-page/hero.tsx
@@ -1,12 +1,13 @@
+import TotalVisits from "../commons/total-visits";
 import UserCard from "../commons/user-card";
 import Button from "../ui/button";
 import TextInput from "../ui/text-input";
 
 export default function Hero() {
     return (
-        <div className="flex border h-screen">
+        <div className="flex h-screen">
             {/*hero-titulo*/}
-            <div className="w-full flex flex-col gap-2 mt-[35vh] border">
+            <div className="w-full flex flex-col gap-2 mt-[35vh]">
                 <h1 className="text-5xl font-bold text-white leading-[64px]">Seus projetos e redes sociais em um único link</h1>
                 <h2 className="text-xl leading-6">Crie sua própria página de projetos e compartilhe eles com o mundo
                     <br />
@@ -22,13 +23,13 @@ export default function Hero() {
             <div className="w-full flex items-center justify-center bg-[radial-gradient(circle_at_50%_50%,#4B2DBB,transparent_55%)]">
                 <div className="relative">
                     <UserCard/>
-                    <div className="absolut -bottom-[7%] -right-[45%]">
-                        {/*<TotalVisits/>*/}
+                    <div className="absolute -bottom-[7%] -right-[45%]">
+                        <TotalVisits/>
                     </div>
-                    <div className="absolut top-[20%] -left-[45%] -z-10">
+                    <div className="absolute top-[20%] -left-[45%] -z-10">
                         {/*<ProjectCard/>*/}
                     </div>
-                    <div className="absolut -top-[5%] -left-[55%] -z-10">
+                    <div className="absolute -top-[5%] -left-[55%] -z-10">
                         {/*<ProjectCard/>*/}
                     </div>
                 </div>
